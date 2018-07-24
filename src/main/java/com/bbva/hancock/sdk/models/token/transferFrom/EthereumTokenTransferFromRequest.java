801b27d81b343213c1e55ff688b561a7c0988b59
Angel Manzano <angel.manzano.contractor@bbva.com>
2018-07-24 16:15:35 +0200

    added tokenTransferFrom

---
+++ src/main/java/com/bbva/hancock/sdk/models/token/transferFrom/EthereumTokenTransferFromRequest.java
@@ -0,0 +1,18 @@
+package com.bbva.hancock.sdk.models.token.transferFrom;
+
+import com.bbva.hancock.sdk.models.token.transfer.EthereumTokenTransferRequest;
+
+public class EthereumTokenTransferFromRequest extends EthereumTokenTransferRequest {
+
+    private String addressOrAlias;
+    private String sender;
+
+    public EthereumTokenTransferFromRequest(String from, String sender, String to, String value, String addressOrAlias) {
+        super(from, to, value, addressOrAlias);
+        this.sender = sender;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+}
