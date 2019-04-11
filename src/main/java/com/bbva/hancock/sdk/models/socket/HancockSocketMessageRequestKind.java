745813aa26cce2d5771d22aaae47a596d4260929
sendoa.moronta <sendoa.moronta.next@bbva.com>
2019-04-11 12:16:38 +0200

    Change sc transactions events

--- src/main/java/com/bbva/hancock/sdk/models/socket/HancockSocketMessageRequestKind.java
+++ src/main/java/com/bbva/hancock/sdk/models/socket/HancockSocketMessageRequestKind.java
@@ -3,21 +3,21 @@ package com.bbva.hancock.sdk.models.socket;
 import lombok.Getter;
 
 public enum HancockSocketMessageRequestKind {
-    WATCHTRANSFER ("watch-transfers"),
-    WATCHTRANSACTION ("watch-transactions"),
-    WATCHSMARTCONTRACT ("watch-contracts"),
-    WATCHSMARTCONTRACTTRANSACTION ("watch-contract-transactions"),
-    WATCHSMARTCONTRACTEVENT ("watch-contracts-events"),
-    UNWATCHTRANSFER ("unwatch-transfers"),
-    UNWATCHTRANSACTION ("unwatch-transactions"),
-    UNWATCHSMARTCONTRACT ("unwatch-contracts"),
-    UNWATCHSMARTCONTRACTTRANSACTION ("unwatch-contract-transactions"),
-    UNWATCHSMARTCONTRACTEVENT ("unwatch-contracts-events");
+    WATCHTRANSFER("watch-transfers"),
+    WATCHTRANSACTION("watch-transactions"),
+    WATCHSMARTCONTRACT("watch-contracts"),
+    WATCHSMARTCONTRACTTRANSACTION("watch-contracts-transactions"),
+    WATCHSMARTCONTRACTEVENT("watch-contracts-events"),
+    UNWATCHTRANSFER("unwatch-transfers"),
+    UNWATCHTRANSACTION("unwatch-transactions"),
+    UNWATCHSMARTCONTRACT("unwatch-contracts"),
+    UNWATCHSMARTCONTRACTTRANSACTION("unwatch-contracts-transactions"),
+    UNWATCHSMARTCONTRACTEVENT("unwatch-contracts-events");
 
     @Getter
     private final String kind;
 
-    HancockSocketMessageRequestKind(String kind){
+    HancockSocketMessageRequestKind(final String kind) {
         this.kind = kind;
     }
 
